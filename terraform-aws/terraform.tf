c3dde9e094ffd29377a3ea2f8b1934464d8edb22
matiasg1200 <matiasg1200@gmail.com>
2023-12-11 14:21:57 +0000

    adding provider configurations on terraform file

--- terraform-aws/terraform.tf
+++ terraform-aws/terraform.tf
@@ -1,18 +1,26 @@
 # terraform configuration
 terraform {
-    required_providers {
-      aws = {
-        version = "~>5.25.0"
-        source  = "hashicorp/aws"
-      }
+  required_providers {
+    aws = {
+      version = "~>5.25.0"
+      source  = "hashicorp/aws"
     }
-    required_version = "~>1.6.4"
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">4.0.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">2.3.0"
+    }
+  }
+  required_version = "~>1.6.4"
 
-    backend "s3" {
-      bucket = "tfstate-remote-s3-bucket-lab"
-      key = "global/terraform.tfstate"
-      region = "us-west-1"
-    }
+  backend "s3" {
+    bucket = "tfstate-remote-s3-bucket-lab"
+    key    = "global/terraform.tfstate"
+    region = "us-west-1"
+  }
 
 }
 
